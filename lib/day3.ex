11b2471b9d10a75c3d71b3da9cb6a9f27d30f5e2
Luke Sanwick <lsanwick@gmail.com>
2017-12-07 21:38:16 -0800

    Alter Day3 to support second part of the problem.

--- lib/day3.ex
+++ lib/day3.ex
@@ -9,18 +9,39 @@ defmodule Day3 do
     Get the Manhattan distance for a given number
 
     ## Examples
-    iex> Day3.get_distance(1)
-    0
-    iex> Day3.get_distance(12)
-    3
-    iex> Day3.get_distance(23)
-    2
-    iex> Day3.get_distance(1024)
-    31
+      iex> Day3.get_distance(1)
+      0
+
+      iex> Day3.get_distance(12)
+      3
+
+      iex> Day3.get_distance(23)
+      2
+
+      iex> Day3.get_distance(1024)
+      31
   """
   def get_distance(num) do
     {_, _, {x, y}} = build_matrix(num)
     abs(x) + abs(y)
+  end
+
+  @doc """
+    Get the next largest value in the spiral sequence when summing neighbors (part 2)
+
+    ## Examples
+    iex> Day3.get_largest_value(325489)
+    330785
+  """
+  def get_largest_value(num) do
+    Process.register(self(), :listener)
+    pid = spawn(Day3, :build_matrix, [num, num])
+    receive do
+      {:result, value} ->
+        Process.exit(pid, :kill)
+        Process.unregister(:listener)
+        value
+    end
   end
 
   @doc """
@@ -33,18 +54,27 @@ defmodule Day3 do
       iex> Day3.build_matrix(2)
       {%{{0, 0} => 1, {1, 0} => 2}, :right, {1, 0}}
   """
-  def build_matrix(cell_num) when cell_num > 1 do
-    {positions, direction, position} = build_matrix(cell_num - 1)
+  def build_matrix(cell_num, max \\ nil) when cell_num > 1 do
+    {positions, direction, position} = build_matrix(cell_num - 1, max)
     if left_filled(positions, direction, position) do
       position = move(direction, position)
     else
       direction = turn_left(direction)
       position = move(direction, position)
     end
-    positions = Map.put(positions, position, cell_num)
+    if max do
+      # Store cell_num as the value for Day 3 part 1, and use the `cell_value` func below for part 2
+      value = cell_value(positions, position)
+      if value > max do
+        send(:listener, {:result, value})
+      end
+    else
+      value = cell_num
+    end
+    positions = Map.put(positions, position, value)
     {positions, direction, position}
   end
-  def build_matrix(1), do: {%{{0,0} => 1}, :down, {0,0}}
+  def build_matrix(1, _), do: {%{{0,0} => 1}, :down, {0,0}}
   @doc """
     Check if the position to the left is filled
 
@@ -63,6 +93,30 @@ defmodule Day3 do
       {:ok, _ } -> true
       :error -> false
     end
+  end
+
+  @doc """
+    Get the cell value for part 2 of the problem (part 1 has no calculation)
+
+    ## Examples
+      iex> Day3.cell_value(%{{0, 0} => 1, {1, 0} => 1, {1, 1} => 2}, {0, 1})
+      4
+  """
+  def cell_value(_, {0,0}), do: 1
+  def cell_value(positions, position) do
+    {x, y} = position
+    neighbors = [
+      {x-1, y+1},
+      {x, y+1},
+      {x+1, y+1},
+      {x+1, y},
+      {x+1, y-1},
+      {x, y-1},
+      {x-1, y-1},
+      {x-1, y},
+    ]
+    values = for neighbor <- neighbors, do: Map.get(positions, neighbor, 0)
+    Enum.sum(values)
   end
 
   def move(:up, {x, y}), do: {x, y+1}

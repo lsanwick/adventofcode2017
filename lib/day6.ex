71ac4a1e10bcd4237ce073e2ebed9add854b28cc
Luke Sanwick <lsanwick@gmail.com>
2018-02-06 09:37:38 -0800

    Finish implementation on Day6

--- lib/day6.ex
+++ lib/day6.ex
@@ -7,22 +7,33 @@ defmodule Day6 do
       {[2, 4, 1, 2], 5}
 
       iex> Day6.redistribute([14, 0, 15, 12, 11, 11, 3, 5, 1, 6, 8, 4, 9, 1, 8, 4])
-      {[2, 4, 1, 2], 5}
+      {[14, 13, 12, 11, 9, 8, 8, 6, 6, 4, 4, 3, 1, 1, 0, 12], 11137}
+
+      iex> Day6.redistribute([14, 13, 12, 11, 9, 8, 8, 6, 6, 4, 4, 3, 1, 1, 0, 12])
+      {[14, 13, 12, 11, 9, 8, 8, 6, 6, 4, 4, 3, 1, 1, 0, 12], 1037}
   """
   def redistribute(banks, history \\ %{}, count \\ 0) do
+    history =
+      if count == 0 do
+        Map.put(history, banks, true)
+      else
+        history
+      end
+
     largest = Enum.max(banks)
-    IO.puts largest
-    index = Enum.find_index(banks, fn(bank) -> bank == largest end)
-    IO.puts index
-    chunks = length(banks) - 1
-    amount_per_chunk = div(largest, chunks)
+    index = Enum.find_index(banks, fn bank -> bank == largest end)
+
     result =
-      banks
-      |> Stream.with_index
-      |> Enum.map(fn({val, i}) -> if i == index, do: rem(largest, chunks), else: val + amount_per_chunk end)
-    IO.inspect result
+      1..largest
+      |> Enum.map(fn offset -> rem(index + offset, length(banks)) end)
+      |> Enum.reduce(List.wrap(banks), fn index, acc ->
+        List.replace_at(acc, index, Enum.at(acc, index) + 1)
+      end)
+
+    result = List.replace_at(result, index, Enum.at(result, index) - largest)
+
     if Map.has_key?(history, result) do
-      { result, count + 1 }
+      {result, count + 1}
     else
       history = Map.put(history, result, true)
       redistribute(result, history, count + 1)
